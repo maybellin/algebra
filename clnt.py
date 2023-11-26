e8858f6ac5dd817498df70ba02a8836237320513
maybellin <63143973+maybellin@users.noreply.github.com>
2023-11-26 23:32:57 +0300

    Add files via upload

---
+++ clnt.py
@@ -0,0 +1,11 @@
+import socket
+import time
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+client.bind(("", 37020))
+while True:
+    data, addr = client.recvfrom(1024)
+    print("received message: %s" % data)
+    time.sleep(1)
